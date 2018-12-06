03d1a292ca92c712d2553a29d6e286427da5bbbf
Imam Fauzan <imamfzn@gmail.com>
2018-12-06 12:46:10 +0700

    Add documentation

--- repository.go
+++ repository.go
@@ -2,9 +2,18 @@ package bukaresep
 
 // Repository is an interface as data access to a persistent storage
 type Repository interface {
+	// Get will return a recipe by particular id from data store
 	Get(id int) (*Recipe, error)
+
+	// Get all will retrieve all recipe from data store
 	GetAll() ([]*Recipe, error)
+
+	// Add will create new recipe to data store
 	Add(recipe *Recipe) (*Recipe, error)
+
+	// Update will modify recipe value to data store
 	Update(recipe *Recipe) error
+
+	// Delete will remove recipe from data store
 	Delete(recipe *Recipe) error
 }
